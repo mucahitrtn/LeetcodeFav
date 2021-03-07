50767d5e203dd14f424df4963e78b961472261ce
mucahitrtn <73777934+mucahitrtn@users.noreply.github.com>
2021-03-07 12:53:50 +0300

    Create TriangleQuest.py

---
+++ TriangleQuest.py
@@ -0,0 +1,13 @@
+for i in range(1,int(input())): 
+    print((10**i)//9*i)
+    
+    
+#   1<= input <= 9
+#   One line solution to print a triangle
+#   1
+#   22  
+#   333 
+#   4444
+#   55555
+#   ...
+#
